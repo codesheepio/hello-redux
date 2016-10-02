690fef8ee9a12893634bf7040c10e5a1a6e1ea75
Supasate Choochaisri <supasate.c@gmail.com>
2016-10-03 02:08:36 +0700

    Remove internal state

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,21 +3,16 @@ import React, { Component } from 'react'
 export default class Counter extends Component {
   constructor(props) {
     super(props)
-    this.state = {
-      counter: 0,
-    }
     this.addCounter = this.addCounter.bind(this)
   }
   addCounter() {
-    this.setState({
-      counter: this.state.counter + 1
-    })
+    this.props.addCounter()
   }
   render() {
     return (
       <div className="counter">
         <h1>{this.props.label}</h1>
-        <h2>{this.state.counter}</h2>
+        <h2>{this.props.counter}</h2>
         <button onClick={this.addCounter}>+1</button>
       </div>
     )
