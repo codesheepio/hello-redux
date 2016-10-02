d4f1100944d9a2ae0ea3ab974b3adc381a51cacb
Supasate Choochaisri <supasate.c@gmail.com>
2016-10-03 02:15:29 +0700

    Simplify component

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react'
 
-const Counter = (props) => {
-  const addCounter = () => {
-    props.addCounter()
-  }
-  return (
-    <div className="counter">
-      <h1>{props.label}</h1>
-      <h2>{props.counter}</h2>
-      <button onClick={addCounter}>+1</button>
-    </div>
-    )
-}
+const Counter = (props) => (
+  <div className="counter">
+    <h1>{props.label}</h1>
+    <h2>{props.counter}</h2>
+    <button onClick={props.addCounter}>+1</button>
+  </div>
+)
 
 export default Counter
