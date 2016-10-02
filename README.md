1c084b9eb8891ae9cbbe6488ea969be7b2286b8c
Supasate Choochaisri <supasate.c@gmail.com>
2016-10-03 01:31:08 +0700

    Init repo

---
+++ README.md
@@ -0,0 +1,4 @@
+# hello-redux
+A repository used in react/redux training bootcamp. It shows how to setup use redux with react.
+## Step by step
+* `npm install` - Install dependency
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Counter from './components/Counter'
+import './style.css'
+
+ReactDOM.render(
+  <Counter label="I'm a counter" />,
+  document.getElementById('react-root')
+)
