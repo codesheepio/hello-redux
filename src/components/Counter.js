dbf5aae64001668dad19b6ea973bf82bee5a0389
Supasate Choochaisri <supasate.c@gmail.com>
2016-10-03 02:13:55 +0700

    Change to functional stateless component

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,20 +1,16 @@
 import React, { Component } from 'react'
 
-export default class Counter extends Component {
-  constructor(props) {
-    super(props)
-    this.addCounter = this.addCounter.bind(this)
+const Counter = (props) => {
+  const addCounter = () => {
+    props.addCounter()
   }
-  addCounter() {
-    this.props.addCounter()
-  }
-  render() {
-    return (
-      <div className="counter">
-        <h1>{this.props.label}</h1>
-        <h2>{this.props.counter}</h2>
-        <button onClick={this.addCounter}>+1</button>
-      </div>
+  return (
+    <div className="counter">
+      <h1>{props.label}</h1>
+      <h2>{props.counter}</h2>
+      <button onClick={addCounter}>+1</button>
+    </div>
     )
-  }
 }
+
+export default Counter
